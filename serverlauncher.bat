3d9cbf7e9ee113f91c02092fc55d9a4a84287276
Mthec <daniel4592@hotmail.com>
2015-12-15 17:19:16 +0000

    Batch file changed to find correct runtime.

--- serverlauncher.bat
+++ serverlauncher.bat
@@ -1,4 +1,10 @@
 
 if exist logging.properties set LOGGING=-Djava.util.logging.config.file=logging.properties
 
-runtime/bin/java "-Dworkdir=%CD%" "-Djava.library.path=%CD%/nativelibs" %LOGGING% -Xmn256M -Xms512m -Xmx2048m -XX:+OptimizeStringConcat -XX:+AggressiveOpts -jar serverlauncher.jar  %*
+IF EXIST runtime (
+    SET run=%~dp0runtime
+) ELSE (
+    SET run=%~dp0../runtime
+)
+
+"%run%/bin/java" "-Dworkdir=%CD%" "-Djava.library.path=%CD%/nativelibs" %LOGGING% -Xmn256M -Xms512m -Xmx2048m -XX:+OptimizeStringConcat -XX:+AggressiveOpts -jar serverlauncher.jar  %*
