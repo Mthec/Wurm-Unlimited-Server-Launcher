ae6dd5ebc12a5dad94b59954ede9aa8b2269e578
Mthec <daniel4592@hotmail.com>
2015-12-17 21:06:34 +0000

    Updated .sh to find runtime folder.

--- serverlauncher.sh
+++ serverlauncher.sh
@@ -2,4 +2,11 @@
 if test -e "logging.properties"; then
 	LOGGING=-Djava.util.logging.config.file=logging.properties
 fi
-./runtime/jre1.8.0_60/bin/java "-Dworkdir=$PWD" "-Djava.library.path=$PWD/nativelibs" $LOGGING -Xmn256M -Xms512m -Xmx2048m -XX:+OptimizeStringConcat -XX:+AggressiveOpts -jar ./serverlauncher.jar "$*"
+
+if test -d "runtime"; then
+    RUNTIME=runtime
+else
+    RUNTIME=../runtime
+fi
+
+LD_LIBRARY_PATH="$PWD/nativelibs" ./$RUNTIME/jre1.8.0_60/bin/java "-Dworkdir=$PWD" "-Djava.library.path=$PWD/nativelibs" $LOGGING -Xmn256M -Xms512m -Xmx2048m -XX:+OptimizeStringConcat -XX:+AggressiveOpts -jar ./serverlauncher.jar "$*"
