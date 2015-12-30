20016c8d38bea7c6dacb7798d820d13e947c899f
Mthec <daniel4592@hotmail.com>
2015-12-30 22:19:03 +0000

    Fixed paths with spaces not working.

--- serverlauncherpatcher.bat
+++ serverlauncherpatcher.bat
@@ -1,7 +1,7 @@
 IF EXIST runtime (
-    SET run=%~dp0runtime
+    "SET run=%~dp0runtime"
 ) ELSE (
-    SET run=%~dp0../runtime
+    "SET run=%~dp0../runtime"
 )
 
 "%run%/bin/java" -classpath serverlauncherpatcher.jar;javassist.jar org.gotti.wurmunlimited.patcher.PatchServerJar
