128fb97992bb7ee71254efd8dd4fcae5016318ed
Mthec <daniel4592@hotmail.com>
2016-01-21 16:31:05 +0000

    Removed old method from TestConsoleReader.

--- src/mod/wurmonline/serverlauncher/consolereader/TestConsoleReader.java
+++ src/mod/wurmonline/serverlauncher/consolereader/TestConsoleReader.java
@@ -5,70 +5,18 @@ import mod.wurmonline.serverlauncher.ServerConsoleController;
 import mod.wurmonline.serverlauncher.ServerController;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Random;
 
 public class TestConsoleReader {
-    static Option[] options;
     static ServerController controller;
-
-    @Deprecated
-    public static void buildMenu() {
-
-        options = new Option[] {
-                new Menu("dostuff", "This is a menu, please type \"hello\"",
-                        new Option[] {
-                                new Command("hello", "something") {
-                                    @Override
-                                    public String action(List<String> tokens) {
-                                        return "Hi there.";
-                                    }
-                                },
-                                new Command("bye", "something else") {
-                                    @Override
-                                    public String action(List<String> tokens) {
-                                        return "I SAID TYPE HELLO!";
-                                    }
-                                }
-                        }),
-                new Command("status", "Check the status of the server.") {
-                    @Override
-                    public String action(List<String> tokens) {
-                        return "*shrugs*";
-                    }
-                },
-                new Command("shutdown", "Shutdown the server.") {
-                    @Override
-                    public String action(List<String> tokens) {
-                        return "Done!";
-                    }
-                },
-                new Command("player_info", "Get information about a player.") {
-                    @Override
-                    public String action(List<String> tokens) {
-                        if (tokens.isEmpty()) {
-                            return "Please provide a player name or id.";
-                        } else {
-                            String[] strings = new String[] {"annoying", "happy"};
-                            Random random = new Random();
-                            return String.join(" ", tokens) + " is " + strings[random.nextInt(2)] + ".";
-                        }
-                    }
-                }
-        };
-    }
 
     public static void start(ServerConsoleController controller) {
         TestConsoleReader.controller = controller;
-        //buildMenu();
-        //(new Thread(new ConsoleReader(controller, options))).start();
         (new Thread(new ConsoleReader(controller))).start();
         // TODO - How to tell when ready to receive commands?
     }
 
     public static void main(String[] args) {
         controller = new FakeController();
-
         start((ServerConsoleController) controller);
     }
 
