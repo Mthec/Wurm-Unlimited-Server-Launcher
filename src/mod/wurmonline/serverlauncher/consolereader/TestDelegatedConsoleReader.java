76376789fc72d82e5eeec8a762e7d93f7de20c96
Mthec <daniel4592@hotmail.com>
2016-01-21 16:49:33 +0000

    Fixed Delegated reader.  Added some protection to Value.

--- src/mod/wurmonline/serverlauncher/consolereader/TestDelegatedConsoleReader.java
+++ src/mod/wurmonline/serverlauncher/consolereader/TestDelegatedConsoleReader.java
@@ -9,17 +9,15 @@ import org.gotti.wurmunlimited.modloader.interfaces.WurmMod;
 
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 public class TestDelegatedConsoleReader {
     static String MODS_DIR = "mods";
 
     public static void main(String[] args) {
+        List<WurmMod> mods = new ArrayList<>();
         try {
-            List<WurmMod> mods = new ModLoader().loadModsFromModDir(Paths.get(MODS_DIR));
+            mods = new ModLoader().loadModsFromModDir(Paths.get(MODS_DIR));
             ServerHook.createServerHook().addMods(mods);
         } catch (IOException e) {
             e.printStackTrace();
@@ -27,6 +25,7 @@ public class TestDelegatedConsoleReader {
         }
         ServerConsoleController controller = new ServerConsoleController();
         Servers.argumets = new SimpleArgumentParser(new String[0], new HashSet<>());
+        controller.setMods(mods);
         //controller.startDB("Adventure");
         Set<Thread> threads = Thread.getAllStackTraces().keySet();
         for (Thread t : threads) {
--- src/mod/wurmonline/serverlauncher/consolereader/Value.java
+++ src/mod/wurmonline/serverlauncher/consolereader/Value.java
@@ -23,7 +23,13 @@ public abstract class Value extends Command {
     public String action(List<String> tokens) {
         try {
             // TODO - Change to get server when gui separation is complete.
+            if (!controller.isInitialized()) {
+                return messages.getString("no_server_selected");
+            }
             ServerEntry current = Servers.localServer;
+            if (current == null) {
+                return messages.getString("server_not_found");
+            }
             if (tokens.isEmpty()) {
                 return MessageFormat.format(messages.getString("current_value"), this.getName(), this.get(current));
             } else {
